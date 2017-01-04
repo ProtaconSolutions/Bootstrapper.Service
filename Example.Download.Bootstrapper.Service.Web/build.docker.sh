25443009f3526ddb45c38aff2fc85fd46bc94d3e
Savpek <savolainen.pekka@gmail.com>
2017-01-04 11:42:16 +0200

    Test versions of jenkings/container build.

---
+++ Example.Download.Bootstrapper.Service.Web/build.docker.sh
@@ -0,0 +1,16 @@
+if [ -z "$1" ]
+  then
+    echo "Usage: ./build.docker.sh [image-tag]"
+    exit 1
+fi
+
+DOCKER_IMAGE_NAME="build/temp/$1"
+
+docker build --no-cache -t ${DOCKER_IMAGE_NAME} -f 'Dockerfile.build' .
+
+CONTAINER_ID=$(docker run -d ${DOCKER_IMAGE_NAME})
+
+docker cp ${CONTAINER_ID}:"/var/download.bootstrapper.service/bin/Debug/netcoreapp1.0" ./dist/
+
+docker rm -f ${CONTAINER_ID} || true
+docker rmi -f ${DOCKER_IMAGE_NAME} || true
